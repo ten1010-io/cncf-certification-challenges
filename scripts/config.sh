ea43c70303627ba16f5b82fde2db141c272bac43
Joonseo Lee <niceweather94@gmail.com>
2026-09-02 14:46:29 +0900

    chore: 저장소 이름 변경 반영 (cncf-certification-challenges)

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,3 @@
 # README 생성기·링크에 쓰는 저장소 설정. GitHub 에 올린 뒤 REPO 를 실제 값으로 바꾼다.
-REPO="${REPO:-ten1010-io/cncf-certification}"
+REPO="${REPO:-ten1010-io/cncf-certification-challenges}"
 BRANCH="${BRANCH:-main}"
